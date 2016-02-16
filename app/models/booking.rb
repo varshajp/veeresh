2dea603ab3ef85c01c48b84088dc0daa7a2be9b4
varsha-bharath <varsha.jp3@gmail.com>
2016-02-17 00:21:49 +0530

    some changes done in views

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,16 @@ has_many :veeresh_accounts
 has_many :online_bookings
 has_many :movies, through: :online_bookings
 belongs_to :cinema
-validates_presence_of :day,:type,:no_of_seats,:t_c
+validates_presence_of :day,:number_of_seats,:t_c
+validate :date_of_booking
+private
+	
+	def date_of_booking
+		if !self.day.nil?
+			if self.day < Date.today
+				errors.add(:day)
+			end
+		end
+	end
 end
+
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,16 @@ belongs_to :review
 has_many :online_bookings 
 has_many :bookings, through: :online_bookings
 validates_presence_of :name,:language,:duration,:release_date
+validate :check_date
+private
+	def check_date
+		if !self.release_date.nil?
+			if self.release_date > Date.tomorrow
+				errors.add(:release_date," should Not be greater than tomorrow")
+			end
+		end
 	end
+		
+end
+
+	
